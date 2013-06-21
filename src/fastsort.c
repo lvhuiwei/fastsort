23591a2a17a7c4065ebbf2790051a5f25091dc35
Huiwei Lu <lvhuiwei@gmail.com>
2013-06-21 11:38:01 +0800

    update

--- src/fastsort.c
+++ src/fastsort.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "mpi.h"
 
 int main(int argc, char *argv[])
@@ -7,10 +8,27 @@ int main(int argc, char *argv[])
     int myid, numprocs;
     MPI_Comm_size(comm, &numprocs);
     MPI_Comm_rank(comm, &myid);
+
+    FILE * fp_r;
+    long fsize;
+    char * buffer;
+    size_t result;
+    fp_r = fopen ( "input" , "rb" );
+    if (fp_r==NULL) {fputs ("File error",stderr); exit (1);}
+    fseek (fp_r , 0 , SEEK_END);
+    fsize = ftell (fp_r);
+    rewind (fp_r);
+    buffer = (char*) malloc (sizeof(char)*fsize);
+    if (buffer == NULL) {fputs ("Memory error",stderr); exit (2);}
+    result = fread (buffer,1,fsize,fp_r);
+    if (result != fsize) {fputs ("Reading error",stderr); exit (3);}
+    /* sort */
+    fclose (fp_r);
+    free (buffer);
+
+    FILE *fp_w;
+    fp_w = fopen("output","wb");
+    fclose (fp_w);
     
-    MPI_File fh;
-    MPI_File_open(comm, "input.txt", MPI_MODE_RDWR, MPI_INFO_NULL, &fh);
-    MPI_File_close(&fh);
-    MPI_Finalize();
     return 0;
 }
